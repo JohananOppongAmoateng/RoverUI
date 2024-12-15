dac5922a3bd7985ea46ab3a0f28766fb6eb0fd19
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2024-12-15 21:11:00 +0000

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9db7k$lun(v-$3j%ol$mivek-&l_vswm%1ho*z!$xac6oko^ou
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['roverui.onrender.com','127.0.0.1','localhost']
 
 
 # Application definition
